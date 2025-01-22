08d2fdfef9325cfed5885ec9311a9eef0f7aa404
Ajumobi isaac <ajumobiisaac96@gmail.com>
2025-01-22 23:47:25 +0100

    #favicon.icon

--- myhrmproject/vite.config.js
+++ myhrmproject/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG'], // Add this line
+  base: '/', // Leave as '/' unless deploying to a subdirectory
 });
